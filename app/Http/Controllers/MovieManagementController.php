7ee5f4991e8bcf4d6efbfa040f304088c8776572
defvin <defvinn@gmail.com>
2024-06-08 22:20:40 +0700

    menambahkan insert data film dan menampilkan data film

--- app/Http/Controllers/MovieManagementController.php
+++ app/Http/Controllers/MovieManagementController.php
@@ -3,8 +3,10 @@
 namespace App\Http\Controllers;
 
 use App\Models\Theater;
+use App\Models\RegisteredMovies;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Validator;
+use Illuminate\Http\RedirectResponse;
 
 class MovieManagementController extends Controller
 {
@@ -13,23 +15,71 @@ class MovieManagementController extends Controller
      */
     public function index()
     {
+        
         return view('movie.dashboard-movies',['title'=>'Movie Management']);
     }
 
     //start of movies controlling
     public function movies()
     {
+        $data_movie = RegisteredMovies::all();
         $title = "New Movies | Movie Management";
-        return view('movie.index',compact('title'));
+        return view('movie.index',compact('title', 'data_movie'));
     }
     public function create_movies()
     {
         $title = "New Movies | Movie Management";
         return view('movie.create-film',compact('title'));
     }
+    public function edit_movies($id){
+        $title = "Edit Movies | Movie Management";
+        $data_movie = RegisteredMovies::find($id);
+        return view('movie.edit-film',compact(['title','data_movie']));
+    }
+    public function store_movies(Request $request){
+        $data = $request->validate([
+            'poster' => 'required|image|file|max:1024|mimes:jpeg,jpg,png',
+            'judul' => 'required',
+            'sutradara' => 'required',
+            'produser'  => 'required',
+            'bahasa'  => 'required',
+            'bahasa_sub'  => 'required',
+            'genre'  => 'required',
+            'sensor'  => 'required',
+            'mulai_tayang'  => 'required',
+            'selesai_tayang' => 'required',
+            'deskripsi' => 'required',
+            'status' => 'required',
+            'diterima' => 'required'
+        ]);
 
+        $data['poster'] = $request->file('poster')->store('movies-poster');
+        $data['judul'] = $request->judul;
+        $data['sutradara'] = $request->sutradara;
+        $data['produser'] = $request->produser;
+        $data['bahasa'] = $request->bahasa;
+        $data['bahasa_subtitle'] = $request->bahasa_sub;
+        $data['genre'] = $request->genre;
+        $data['sensor'] = $request->sensor;
+        $data['show_start'] = $request->mulai_tayang;
+        $data['show_end'] = $request->selesai_tayang;
+        $data['deskripsi'] = $request->deskripsi;
+        $data['status_approval'] = $request->status;
+        $data['tanggal_approval'] = $request->diterima;
+
+        RegisteredMovies::create($data);
+
+        //redirect to index
+        return redirect()->route('movie-movies')->with('success', 'Film Berhasil Ditambahkan!');
+    
+    }
     //end of movies controlling
 
+    public function delete_movies($id)
+    {
+        RegisteredMovies::where('id_movie', $id)->delete();
+        return redirect()->route('movie-movies')->with('success', 'Film Berhasil Dihapus!');
+    }
     /**
      * below is Theather Controlling section
     */
@@ -47,11 +97,11 @@ class MovieManagementController extends Controller
     }
     public function edit_theater($id){
         $title = "Edit Theater | Movie Management";
-        $data_studio = Theater::find($id);
-        return view('theater.edit',compact(['title','data_studio']));
+        $data_studio = Theater::where('theater_id',$id)->first();
+        return view('theater.edit',compact(['title','data_studio']))->with('data',$data_studio);
     }
 
-    public function store_theater(Request $request){
+    public function store_theater(Request $request){    
         $validator = Validator::make($request->all(),[
            'nama_theater' => 'required',
            'status' => 'required'
